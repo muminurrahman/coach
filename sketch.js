78af234c658cd8efd32356c970a0ff6f8fe79679
muminurrahman <mumin.rahman@outlook.com>
2023-09-17 12:34:54 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let voice;
+
+function setup() {
+  voice = new p5.Speech();
+  voice.setLang("en-US");
+  voice.setRate(1.0);
+  call();
+}
+
+function draw() {}
+
+function call() {
+  const direction = ["Left", "Right"];
+  const boxingMoves = ["Jab", "Cross", "Hook", "Uppercut", "Slip", "Roll"];
+
+  const sessionDuration = 60 * 1000; // 60 seconds
+  const intervalMin = 500; // Minimum interval in milliseconds
+  const intervalMax = 2000; // Maximum interval in milliseconds
+
+  const startTime = Date.now();
+  let endTime = startTime + sessionDuration;
+
+  const boxingInterval = () => {
+    const currentTime = Date.now();
+    if (currentTime < endTime) {
+      const randomDirection = direction[Math.floor(Math.random() * direction.length)];
+      const randomMove = boxingMoves[Math.floor(Math.random() * boxingMoves.length)];
+      voice.speak(randomDirection + " " + randomMove);
+      console.log(randomDirection + " " + randomMove);
+
+      const randomInterval = Math.random() * (intervalMax - intervalMin) + intervalMin;
+      setTimeout(boxingInterval, randomInterval);
+    } else {
+      console.log("Session complete.");
+    }
+  };
+
+  // Start the first boxing callout
+  boxingInterval();
+}
